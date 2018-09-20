0349204e0c0427e4eb46c8b501a46d2914f8ec47
Sanden Averett <sanden@sandenaverett.com>
2018-09-20 20:45:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if string.toLowerCase() === string {
+  if (string.toLowerCase() === string) {
     return "I can\'t hear you!";
-  } else if string.toUpperCase() === string {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
-  } else string === "I love you, Grandma." {
+  } else (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
